a776f243e9ee8298897c25ba2ce8f6b5befa912c
Abdulaziz Alsoliman <a.alsoliman@gmail.com>
2013-06-16 18:02:53 -0700

    Latest update

---
+++ gen/cgu/edu/ist380/solimana/Project/AConnect/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cgu.edu.ist380.solimana.Project.AConnect;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
